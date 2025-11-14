5440eae5db44e4dc31ff062d0dfe21ce3ee2bed2
ahillahaffat <kammala88@gmail.com>
2025-11-14 11:36:10 +0700

    change image on about section

--- apps/gs-app/src/app/pages/home/sections/aboutSection.tsx
+++ apps/gs-app/src/app/pages/home/sections/aboutSection.tsx
@@ -15,14 +15,14 @@ const solutions = [
     description:
       'Berperan sebagai dasar dalam membangun solusi konstruksi dengan teknologi terbaru, yang memfasilitasi pembuatan model bangunan yang lebih efisien dan presisi.',
     color: 'var(--color-primary)',
-    image: '/images/geom.jpg',
+    image: '/images/geo1.jpg',
   },
   {
     title: 'Geomatika',
     description:
       'Berfokus pada pengolahan data spasial, memastikan bahwa semua langkah perencanaan dan eksekusi proyek dimulai dengan informasi yang akurat dan terperinci.',
     color: 'var(--color-primary-dark)',
-    image: '/images/geo.jpg',
+    image: '/images/geo2.jpg',
   },
 ];
 
